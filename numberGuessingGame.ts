452e178ceffb106fa831302163118653dd8486af
AkramQureshi <161283057+AkramQureshi@users.noreply.github.com>
2024-04-28 20:41:25 +0500

    Add files via upload

---
+++ numberGuessingGame.ts
@@ -0,0 +1,14 @@
+let guessNumber = Math.random() * 3
+let roundNumber = Math.floor(guessNumber)
+
+console.log("computer Number is = ", roundNumber)
+console.log("Your Guessing Number is = 1 ")
+
+if (roundNumber==1) 
+    {
+console.log("Congratulations.... You Won")
+}
+else
+{console.log("You Loss... Try Again")
+
+}
